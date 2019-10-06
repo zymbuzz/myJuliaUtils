e7977c29e79cd93dc474fe1b587cf44eee4bcaa0
zymbuzz <zymantasbud@gmail.com>
2019-10-06 14:48:26 +0200

    first

---
+++ src/myJuliaUtils.jl
@@ -0,0 +1,5 @@
+module myJuliaUtils
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using myJuliaUtils
+using Test
+
+@testset "myJuliaUtils.jl" begin
+    # Write your own tests here.
+end
