e0a5c0eb180a0948bb5b93627bd1732b48cd8298
zymbuzz <zymantasbud@gmail.com>
2019-10-06 14:50:34 +0200

    my functions

--- src/myJuliaUtils.jl
+++ src/myJuliaUtils.jl
@@ -1,5 +1,10 @@
 module myJuliaUtils
 
-greet() = print("Hello World!")
+# extfunc.jl
+lag0, eye, vech, vec2sym, vec2ltri, vec2ltriW1, stabcheck, stabcheckC, preparexy, nanmean,
+wish, iwish, cholPSD, randnPSD, genPSDmatStrict, genPDmat, genPSDmat, simMC, quantileArr, companionf, 
+ismyapprox, normpdf, acf, decVCV, ols1, inbetween
+
+include("extfunc.jl");
 
 end # module
