680041aaa0b9767a72bc630ecea4cc434d98bdac
zymbuzz <zymantasbud@gmail.com>
2019-10-06 16:05:40 +0200

    small fix

--- src/myJuliaUtils.jl
+++ src/myJuliaUtils.jl
@@ -10,7 +10,7 @@ export
 # extfunc.jl
 lag0, eye, vech, vec2sym, vec2ltri, vec2ltriW1, stabcheck, stabcheckC, preparexy, nanmean,
 wish, iwish, cholPSD, randnPSD, genPSDmatStrict, genPDmat, genPSDmat, simMC, quantileArr, companionf, 
-ismyapprox, normpdf, acf, decVCV, ols1, inbetween
+ismyapprox, normpdf, acf, decVCV, ols1, inbetween,
 # evalF.jl
 useN2fit, useMvN2fit
 
