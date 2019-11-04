0d4c5c763bd63b30484013c97e3c3d2c98d3c43d
zymbuzz <zymantasbud@gmail.com>
2019-11-04 17:26:57 +0100

    adding stronger test

--- test/test_generate.jl
+++ test/test_generate.jl
@@ -6,13 +6,13 @@ using Statistics
 
 @testset "testing genLPM function - Monte Carlo test" begin
 T = 10000
-K = 100000
+K = 1000000
 X = [ones(T) randn(T)];
 lammbda = [rand(),randn() / 10.]
 
 lambbdaMean = 0. .* lammbda
 y = BitArray(undef, T)
-for i = 1:K
+@time for i = 1:K
     y .= genLPM(lammbda, T, X)
     lambbdaMean .+= X \ y
 end
