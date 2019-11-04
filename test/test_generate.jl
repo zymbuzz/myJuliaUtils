254349b65adabe1c22401d89fa775a0b69955f3e
zymbuzz <zymantasbud@gmail.com>
2019-11-04 18:48:28 +0100

    small

--- test/test_generate.jl
+++ test/test_generate.jl
@@ -5,19 +5,19 @@ using DelimitedFiles
 using Statistics
 
 @testset "testing genLPM function - Monte Carlo test" begin
-T = 10000
-K = 200000
-X = [ones(T) randn(T)];
-lammbda = [rand(),randn() / 10.]
+    T = 10000
+    K = 200000
+    X = [ones(T) randn(T)];
+    lammbda = [rand(),randn() / 10.]
 
-lambbdaMean = 0. .* lammbda
-y = BitArray(undef, T)
-@time for i = 1:K
-    y .= genLPM(lammbda, T, X)
-    lambbdaMean .+= X \ y
-end
-lambbdaMean ./= K
+    lambbdaMean = 0. .* lammbda
+    y = BitArray(undef, T)
+    @time for i = 1:K
+        y .= genLPM(lammbda, T, X)
+        lambbdaMean .+= X \ y
+    end
+    lambbdaMean ./= K
 
-@test isapprox(lambbdaMean, lammbda, atol = 0.01)
+    @test isapprox(lambbdaMean, lammbda, atol = 0.01)
 
 end
